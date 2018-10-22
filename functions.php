d386d6b24f38fbc12a8eef6406f592be2430274c
Lukas <lukas.chrap@gmail.com>
2018-10-22 21:31:20 +0300

    10.22 skaiciuokle

--- functions.php
+++ functions.php
@@ -1,11 +1,25 @@
 <?php
 
-function getFeel($t) {
-	if ($t > 30) {
-		return "karsta";
-	} else if ($t > 15) {
-		return "silta";
-	} else if ($t >= 5 && $t <= 14) {
-		return "vesu";
+function shipping($cp) {
+	if ($cp < 40) {
+		return 10;
+	} else {
+		return 0;
 	}
+}
+
+function margin($cp) {
+	if ($cp < 100) {
+		return $cp * 0.3;
+	} else {
+		return $cp * 0.2;
+	}
+}
+
+function vat($cp) {
+	return ($cp + shipping($cp) + margin($cp)) * 0.21;
+}
+
+function retail($cp) {
+	return $cp + shipping($cp) + margin($cp) + vat($cp);
 }
--- index.php
+++ index.php
@@ -6,13 +6,33 @@ include('functions.php');
 
 <!DOCTYPE html>
 <html>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <head>
 	<title>PHP</title>
 </head>
 <body>
+	<div class="container">
+		<table class="table table-bordered table-dark">
+			<thead>
+				<tr>
+					<th scope="col">Cost Price</th>
+					<th scope="col">Shiping</th>
+					<th scope="col">Margin</th>
+					<th scope="col">VAT</th>
+					<th scope="col">Retail</th>
+				</tr>
+			</thead>
+			<tbody>
 
-	<?="Šiandien lauke yra " . getFeel(10) . "<br /";?>
+				<?php 
+				for ($cp = 10; $cp <= 150; $cp += 10) { 
+					echo "<tr><td>$cp</td><td>" . shipping($cp) . "</td><td>" . margin($cp) . "</td><td>" . vat($cp) . "</td><td>" . retail($cp) . "</td></tr>";
+				}
+				?>		
 
-</body>
-</html>
+
+			</tbody>
+		</table>
+	</body>
+	</html>
+<!-- <?="Šiandien lauke yra " . getFeel(10) . "<br /";?> -->
